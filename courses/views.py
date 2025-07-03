eedf4d78ff698f24303a99a876dd362683d2f7cb
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-03 19:47:54 +0800

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Course
 
 def course_list(request):
+    print("🌀 This view has been updated.")  # ✅ Add this line here
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
