4fcc70afadee3e8c574b92d2fe79c83f43a4e528
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-07 18:40:57 +0800

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
 
-#urlpatterns = [
-#    path('', views.course_list, name='course_list'),
-#]
 urlpatterns = [
-    path('', views.dashboard_home, name='dashboard_home'),  # default homepage
-    path('courses/', views.course_list, name='course_list'),
+    path('', views.course_list, name='course_list'),  # will be at /courses/
 ]
