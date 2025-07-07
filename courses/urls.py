bd9758d6b35bd4a39a5726588761fbae48d92480
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-07 20:01:58 +0800

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.course_list, name='course_list'),  # will be at /courses/
+    path('run-migrations/', views.run_migrations),  # temporary route
 ]
